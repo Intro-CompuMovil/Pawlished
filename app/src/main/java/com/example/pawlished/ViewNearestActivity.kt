dea136e2484621934031295c4961f2a2420e1c22
Darve99 <104438377+Darve99@users.noreply.github.com>
2023-09-12 13:48:11 -0500

    map

--- app/src/main/java/com/example/pawlished/ViewNearestActivity.kt
+++ app/src/main/java/com/example/pawlished/ViewNearestActivity.kt
@@ -1,17 +1,24 @@
-package com.example.pawlished
-
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import pub.devrel.easypermissions.EasyPermissions
+import pub.devrel.easypermissions.PermissionRequest
 
-class ViewNearestActivity : AppCompatActivity() {
+class ViewNearestActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.view_nearest)
+
         val volverMainButton: Button = findViewById(R.id.volverMainButton)
         val peluquerias = arrayOf(
             "el mundo de la mascota\nDirección: Calle Falsa 123\nHorario: 9:00 AM - 6:00 PM",
@@ -29,6 +36,24 @@ class ViewNearestActivity : AppCompatActivity() {
             finish()
         }
 
+        // Solicitar permiso de ubicación
+        val locationPermission = Manifest.permission.ACCESS_FINE_LOCATION
+        if (EasyPermissions.hasPermissions(this, locationPermission)) {
+            // El permiso ya está concedido, puedes mostrar el mapa real aquí.
+            val mapImageView = findViewById<ImageView>(R.id.mapImageView)
+            mapImageView.setImageResource(R.drawable.real_map_image)
+        } else {
+            // El permiso no está concedido, solicítalo.
+            EasyPermissions.requestPermissions(
+                PermissionRequest.Builder(this, 1, locationPermission)
+                    .setRationale("Necesitamos tu ubicación para mostrar el mapa.")
+                    .setPositiveButtonText("Aceptar")
+                    .setNegativeButtonText("Cancelar")
+                    .setTheme(R.style.AppTheme)
+                    .build()
+            )
+        }
+
         listView.setOnItemClickListener { _, _, position, _ ->
             // Obtén la peluquería seleccionada
             val selectedPeluqueria = peluquerias[position]
@@ -42,5 +67,16 @@ class ViewNearestActivity : AppCompatActivity() {
             intent.putExtra("selected_peluqueria", selectedPeluqueria)
             startActivity(intent)
         }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        // El usuario concedió permisos, muestra el mapa real aquí.
+        val mapImageView = findViewById<ImageView>(R.id.mapImageView)
+        mapImageView.setImageResource(R.drawable.real_map_image)
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        // El usuario denegó permisos, muestra un mensaje o realiza una acción apropiada.
+        // Por ejemplo, puedes mostrar un mensaje indicando que el mapa no se mostrará sin permisos.
     }
 }
