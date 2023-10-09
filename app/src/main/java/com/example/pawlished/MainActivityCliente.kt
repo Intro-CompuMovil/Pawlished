77e27e7014953445bc6bc6a796975953e631b447
Darve99 <104438377+Darve99@users.noreply.github.com>
2023-10-08 22:50:54 -0500

    correcion main cliente

--- app/src/main/java/com/example/pawlished/MainActivityCliente.kt
+++ app/src/main/java/com/example/pawlished/MainActivityCliente.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
@@ -15,9 +16,26 @@ class MainActivityCliente : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_cliente)
-  val toolbar= findViewById<Toolbar>(R.id.toolbar)
+        val solicitarCorteButton: Button = findViewById(R.id.solicitarCorteButton)
+        val verEstadoButton: Button = findViewById(R.id.verEstadoButton)
+        val verPeluqueriasButton: Button = findViewById(R.id.verPeluqueriasButton)
+        val toolbar= findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         mAuth = FirebaseAuth.getInstance()
+        solicitarCorteButton.setOnClickListener {
+            val intent = Intent(this, SolicitarCorteActivity::class.java)
+            startActivity(intent)
+        }
+
+        verEstadoButton.setOnClickListener {
+            val intent = Intent(this, VerEstadoActivity::class.java)
+            startActivity(intent)
+        }
+
+        verPeluqueriasButton.setOnClickListener {
+            val intent = Intent(this, VerPeluqueriasActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
