00f5a6c14af063d14ca4d2bf4bafae903ba1141e
Juaness06 <jesteban.granada@javeriana.edu.co>
2023-10-16 17:58:35 -0500

    añadir la parte de las rutas en el mapa

--- app/src/main/java/com/example/pawlished/MapaActivity.kt
+++ app/src/main/java/com/example/pawlished/MapaActivity.kt
@@ -2,10 +2,13 @@ package com.example.pawlished
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
@@ -18,9 +21,15 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.example.pawlished.databinding.ActivityMapaBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.Polyline
+import com.google.android.gms.maps.model.PolylineOptions
+import org.osmdroid.bonuspack.routing.OSRMRoadManager
+import org.osmdroid.bonuspack.routing.RoadManager
+import org.osmdroid.util.GeoPoint
 
 class MapaActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -29,16 +38,19 @@ class MapaActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mFusedLocationClient: FusedLocationProviderClient
     var latitud = 0.0
     var longitud = 0.0
+    lateinit var roadManager: RoadManager
+    var roadOverlay: Polyline? = null
+    private lateinit var osmMap: MapView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val seleccionarTienda:Button = findViewById(R.id.seleccionarTienda)
-
-
 
         binding = ActivityMapaBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         requestPermission()
@@ -48,8 +60,9 @@ class MapaActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-
-
+        roadManager = OSRMRoadManager(this, "ANDROID")
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
 
 
     }
@@ -112,9 +125,9 @@ class MapaActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
 
-
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+
 
         mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.style_json))
         //setear la localización
@@ -140,19 +153,60 @@ class MapaActivity : AppCompatActivity(), OnMapReadyCallback {
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(miUbicacion))
 
 
-                   //Poner marcadores de las tiendas que
+                    val peluqueriaB = LatLng(4.701935137988523, -74.0286241489743)
+                    mMap.addMarker(
+                        MarkerOptions().position(peluqueriaB).title("Peluquería B")
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                    )
+
+                    val peluqueriaC = LatLng(4.6982475259950975, -74.0363559866194)
+                    mMap.addMarker(
+                        MarkerOptions().position(peluqueriaC).title("Peluquería C")
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                    )
+
+                    val peluqueriaA = LatLng(4.70169077340691, -74.03191386249614)
+                    mMap.addMarker(
+                        MarkerOptions().position(peluqueriaA).title("Peluqueria A")
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+                    )
+
+                    //Cuado se seleccione el marcador de una peluqueríaReal, se debe mostrar la ruta
+                    mMap.setOnMarkerClickListener { marker ->
+
+                        //cada vez que se seleccione un marcador, se debe borrar la ruta anterior
+                        roadOverlay?.remove()
+
+                        val road = roadManager.getRoad(
+                            arrayListOf(
+                                GeoPoint(latitud, longitud),
+                                GeoPoint(marker.position.latitude, marker.position.longitude)
+                            )
+                        )
+                        Log.i("OSM_Activity", "Route lenght: ${road.mLength} klm")
+                        Log.i("OSM_Activity", "Duration: ${road.mDuration / 60} min")
+
+                        val ruta = ArrayList<LatLng>()
+                        for (i in 0 until road.mRouteHigh.size) {
+                            ruta.add(LatLng(road.mRouteHigh[i].latitude, road.mRouteHigh[i].longitude))
+                        }
 
 
+                        val polylineOptions = PolylineOptions()
+                            .addAll(ruta.toMutableList())
+                            .color(Color.RED)
+                            .width(10f)
+
+                        roadOverlay = mMap.addPolyline(polylineOptions)
+                        Toast.makeText(this, "Distancia: ${road.mLength} klm\nDuración: ${road.mDuration / 60} min", Toast.LENGTH_LONG).show()
+                        true
+                    }
                 }
             }
 
         } else {
             requestPermission()
         }
-
-
-
-
 
         mMap.uiSettings.isZoomControlsEnabled = true // Habilitar los "gestures" como "pinch to zoom"
         mMap.uiSettings.isZoomGesturesEnabled = true // Habilitar los botones de zoom
@@ -179,3 +233,4 @@ class MapaActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 }
+
