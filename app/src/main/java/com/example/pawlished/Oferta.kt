27b20ef1089c41ceef01d8dbdb5b2266cf633e4c
Darve99 <104438377+Darve99@users.noreply.github.com>
2023-10-23 17:32:09 -0500

    clase oferta

---
+++ app/src/main/java/com/example/pawlished/Oferta.kt
@@ -0,0 +1,9 @@
+package com.example.pawlished
+
+data class Oferta(
+    val id: String,
+    val solicitudId: String,
+    val precio: String,
+    val estado: String,
+    val peluqueria: String
+)
